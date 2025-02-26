e532e92ed642358415bf0aac764871f11132178d
Paulo Henrique <phdsreis@gmail.com>
2025-02-26 03:10:33 -0300

    switch case

---
+++ src/edu/paulinhoh/javabasico/condicional/SistemaMedidas.java
@@ -0,0 +1,22 @@
+package edu.paulinhoh.javabasico.condicional;
+
+public class SistemaMedidas {
+    public static void main(String[] args) {
+        String sigla = "M";
+
+        switch (sigla) {
+            case "P":
+                System.out.println("PEQUENO");
+                break;
+            case "M":
+                System.out.println("MEDIO");
+                break;
+            case "G":
+                System.out.println("GRANDE");
+                break;
+            default:
+                System.out.println("INDEFINIDO");
+                break;
+        }
+    }
+}
