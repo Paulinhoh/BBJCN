58a38ea239c6e2a957c365b88c79efc34d2843d4
Paulo Henrique <phdsreis@gmail.com>
2025-02-25 23:37:42 -0300

    Java doc

---
+++ src/edu/paulinhoh/javabasico/exemplos/Calculadora.java
@@ -0,0 +1,34 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matematicas entre dois operadores
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe para 
+ * desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Paulo Henrique
+ * @version 1.0
+ * @since 25/02/2025
+ */
+
+package edu.paulinhoh.javabasico.exemplos;
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois numeros inteiros
+     * @param numeroUm este é o primeiro parametro do método
+     * @param numeroDois este é o segundo parametro do método
+     * @return inr é o resultado deste método é a soma dos dois numeros
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm+numeroDois;
+    }
+}
+
+// Documentações Java:
+/*
+ * https://docs.oracle.com/en/java/
+ * https://dev.java/learn/
+ */
+
+//  Java Doc:
+// javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java
