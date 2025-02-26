0c1f0045ec61daffaa0489302f7134aae53068f6
Paulo Henrique <phdsreis@gmail.com>
2025-02-25 23:20:22 -0300

    Palavras reservadas

---
+++ src/edu/paulinhoh/javabasico/PalavrasReservadas.java
@@ -0,0 +1,71 @@
+package edu.paulinhoh.javabasico;
+
+public class PalavrasReservadas {
+    // Controle de pacotes:
+    /*
+     * import: importa pacotes ou classes para dentro do projeto
+     * package: especifica a que pacote todas as classes de um arquivo pertence
+     */
+
+    //  Modificadores de acesso:
+    /*
+     * public: acesso de qualquer classe
+     * private: acesso apenas dentro da classe
+     * protected: acesso por classes no mesmo pacote e subclasses
+     */
+
+    //  Primitivos:
+    /*
+     * boolean
+     * byte
+     * char
+     * double
+     * float
+     * int
+     * log
+     * short
+     * void
+     */
+
+    //  Modificadores de classes, variaveis ou métodos:
+    /*
+     * abstract: classe que não pode ser instaciada ou método que precisa ser implementado por uma subclasse não abstrata
+     * class: especifica uma classe
+     * extends: indica a superclasse que a subclasse esta estendendo 
+     * final: impossibilita que uma classe seja estendida, que um método seja sobrescrito ou que uma variavel seja reiniciada
+     * implements: indica as interfaces que uma classe ira implementar
+     * inteface: especifica uma interface
+     * native: indica que um método esta escrito em uma liguagem dependente de plataforma, tipo C
+     * new: instancia um novo objeto, chamando seu construtor
+     * static: faz um método ou variavel pertencer à classe ao inves de às instâncias
+     * strictfp: usado em frente a um método ou classe para indicar que os números de ponto flutuante seguirão as regras de ponto flutuante em todas as expressões
+     * synchronized: indica que um métodos só pode ser acessado por uma thread de cada vez
+     * transient: impede a serialização de campos
+     * volatile: indica que uma variavel pode ser alterada durante o uso de threads 
+     */
+
+    //  Controle de fluxo dentro de um bloco de código:
+    /*
+     * break: sai do bloco de código em que ele esta
+     * case: executa um bloco de código dependendo do teste do switch
+     * continue: pula a execução do código que viria após esse linha e vai para a proxima passagem do loop
+     */
+
+    //  Tratamento de erros:
+    /*
+     * assert: testa uma expressão condicional para verificar uma suposição do programador
+     * catch: declara o bloco de código usado para tratar uma exceção
+     * finally: bloco de código, após o try-catch, que é executado independentemente do fluxe de programa ao lidar com uma exceção
+     * throw: usado para passar uma exceção para o método que o chamou
+     * throws: indica que um método pode passar uma exceção para o método que chamou
+     * try: bloco de código que tentara ser executado, mas que pode causar uma exceção
+     */
+
+    //  Variaveis de referencia:
+    /*
+     * super: refere-se a superclasse imediata
+     * this: refere-se a instacia atual do objeto
+     */
+
+    // ...
+}
