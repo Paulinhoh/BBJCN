49289ae2673051be837a6846034d3eef146fae56
Paulo Henrique <phdsreis@gmail.com>
2025-03-12 23:16:08 -0300

    update: gitignore

--- src/edu/paulinhoh/poo/colections/set/Ordenacao/exemplo/CadastroProdutos.java
+++ src/edu/paulinhoh/poo/colections/set/Ordenacao/exemplo/CadastroProdutos.java
@@ -29,15 +29,15 @@ public class CadastroProdutos {
     public static void main(String[] args) {
         CadastroProdutos cadastroProdutos = new CadastroProdutos();
 
-        cadastroProdutos.adicionarProduto(1l, "Produto 5", 15d,5);
-        cadastroProdutos.adicionarProduto(2l, "Produto 0", 20d,10);
-        cadastroProdutos.adicionarProduto(1l, "Produto 3", 10d,2);
-        cadastroProdutos.adicionarProduto(9l, "Produto 9", 2d,2);
+        cadastroProdutos.adicionarProduto(1L, "Produto 5", 15d,5);
+        cadastroProdutos.adicionarProduto(2L, "Produto 0", 20d,10);
+        cadastroProdutos.adicionarProduto(1L, "Produto 3", 10d,2);
+        cadastroProdutos.adicionarProduto(9L, "Produto 9", 2d,2);
 
         System.out.println(cadastroProdutos.produtoSet);
 
         System.out.println(cadastroProdutos.exibirProdutosPorNome());
-        System.out.println("");
+        System.out.println();
         System.out.println(cadastroProdutos.exibirProdutosPorPreco());
     }
 }
